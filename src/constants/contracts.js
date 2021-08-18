796b3575d3f2e1922cd98d3db0ef2c8b696b0b64
hhhpw <haopeiwei@qq.com>
2021-08-18 21:13:30 +0800

    修改合约地址

--- src/constants/contracts.js
+++ src/constants/contracts.js
@@ -6,18 +6,19 @@ export const CONTRACTS_ADDRESS = "0x89e1db66b5879bf19f5c1cc7d12d901a";
 export const SMART_CONTRACTS_TEST_API = "https://barnard-seed.starcoin.org"; // 测试网络
 
 // export const SMART_CONTRACTS_API = "https://starcoin.org";
-export const SMART_CONTRACTS_API = "https://barnard-seed.atlaspad.co";
+// export const SMART_CONTRACTS_API = "https://barnard-seed.atlaspad.co";
+export const SMART_CONTRACTS_API = "https://seed.atlaspad.io";
 
 export const STC_PRECISION = 1000000000; // STC精度
 
 export const STAKE_STC_FUNCTION_ID =
-  "0x64c66296d98d6ab08579b14487157e05::OfferingScript::staking";
+  "0x89e1db66b5879bf19f5c1cc7d12d901a::OfferingScript::staking";
 
 export const UNSTAKE_STC_FUNCTION_ID =
-  "0x64c66296d98d6ab08579b14487157e05::OfferingScript::unstaking";
+  "0x89e1db66b5879bf19f5c1cc7d12d901a::OfferingScript::unstaking";
 
 export const PAY_USDT_FUNCTION_ID =
-  "0x64c66296d98d6ab08579b14487157e05::OfferingScript::exchange";
+  "0x89e1db66b5879bf19f5c1cc7d12d901a::OfferingScript::exchange";
 
 export const STAR_MASK_PLUGIN_URL =
   "https://chrome.google.com/webstore/detail/starmask/mfhbebgoclkghebffdldpobeajmbecfk";
